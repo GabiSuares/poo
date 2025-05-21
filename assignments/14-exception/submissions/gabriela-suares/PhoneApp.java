3b2cf5b7766ec62c9bd5bd57d8c403c38950a0bd
GabiSuares <gabriela.suares200@gmail.com>
2025-05-21 12:24:38 -0300

    Entrega atividadee 5

---
+++ assignments/14-exception/submissions/gabriela-suares/PhoneApp.java
@@ -0,0 +1,22 @@
+public class PhoneApp {
+    public static void main(String[] args) {
+        try {
+        Phone phone1 = new Phone("", "1234567890123456");
+        } catch (ValidationException e) {
+        System.out.println("Erro ao criar telefone 1: " + e.getMessage());
+        }
+
+        try {
+        Phone phone2 = new Phone("Telefone 2", "12345");
+        } catch (ValidationException e) {
+            System.out.println("Erro ao criar telefone 2: " + e.getMessage());
+        }
+
+        try {
+        Phone phone3 = new Phone("Telefone 3", "9876543210123456");
+        System.out.println("Telefone criado com sucesso: " + phone3);
+        } catch (ValidationException e) {
+        System.out.println("Erro ao criar telefone 3: " + e.getMessage());
+    }
+}
+}
